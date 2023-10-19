edd6341931e3091d6b62eca9b4e5c62fa5605f19
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2023-10-19 12:36:23 +0200

    First

---
+++ src/Operadores.java
@@ -0,0 +1,8 @@
+public class Operadores {
+  public static void main(String[] args) {
+    String nome1 = "DEYVISON";
+    String nome2 = new String("DEYVISON");
+    boolean result = nome1.equals(nome2);
+    System.out.println(result);
+}
+}
---
+++ src/TiposVariaveis.java
@@ -0,0 +1,18 @@
+public class TiposVariaveis {
+
+  public static void main(String [] args) throws Exception {
+    double sallarioMinimo = 2500;
+    System.out.println("Salário mínimo: " + sallarioMinimo);
+
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short) numeroNormal;
+
+    int numero = 5;
+
+    System.out.println("Número: " + numero);
+
+    final double VALOR_DE_PI = 3.14;
+
+  }
+}
