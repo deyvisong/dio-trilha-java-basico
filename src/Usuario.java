1a3b76d2d7ab43227a7c0562c747f6886d64db1e
cd ~/code/$deyvisong/dotfiles && zsh git_setup.sh <deyvisonalves@protonmail.com>
2023-10-20 12:33:22 +0200

    Method to turn on and turn off tv

---
+++ src/Usuario.java
@@ -0,0 +1,17 @@
+public class Usuario {
+  public static void main(String[] args) throws Exception {
+
+    SmartTv smartTv = new SmartTv();
+
+    System.out.println("TV Ligada ? " + (smartTv.ligada ? "Ligada" : "Desligada"));
+    System.out.println("Canal Atual : " + smartTv.canal);
+    System.out.println("Volume Atual : " + smartTv.volume);
+
+    smartTv.ligar ();
+    System.out.println("TV Ligada ? " + (smartTv.ligada ? "Ligada" : "Desligada"));
+
+    smartTv.desligar();
+    System.out.println("Tv ligada ? " + (smartTv.ligada ? "Ligada" : "Desligada"));
+
+  }
+}
